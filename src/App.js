290521649e34e6586364c9b1b41984b54a06a4d0
dryroque <122841769+dryroque@users.noreply.github.com>
2024-05-11 16:38:00 -0300

    Update: test

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import PaginaInicial from './Paginas/PaginaInicial/PaginaInicial';
 import PaginaCadastro from './Paginas/PaginaCadastro/PaginaCadastro';
+import PaginaLogin from './Paginas/PaginaLogin/PaginaLogin';
 import NavBar from './Componentes/Menu/NavBar';
 
 export default function App() {
   return (
     <>
-      <PaginaCadastro />
+      <PaginaLogin />
     </>
   );
 }
