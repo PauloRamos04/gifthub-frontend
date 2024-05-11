9cc0601e5ef533a6d70d42874618021c3fece8c5
dryroque <122841769+dryroque@users.noreply.github.com>
2024-05-11 16:17:07 -0300

    Feat: creating menu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import PaginaInicial from './Paginas/PaginaInicial/PaginaInicial';
+import NavBar from './Componentes/Menu/NavBar';
 
 export default function App() {
   return (
