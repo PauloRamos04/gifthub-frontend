63ebafb54a717c422a0494015eeea43691b5606c
dryroque <122841769+dryroque@users.noreply.github.com>
2024-05-11 16:33:02 -0300

    Create: Page register

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import PaginaInicial from './Paginas/PaginaInicial/PaginaInicial';
+import PaginaCadastro from './Paginas/PaginaCadastro/PaginaCadastro';
 import NavBar from './Componentes/Menu/NavBar';
 
 export default function App() {
   return (
     <>
-    <PaginaInicial></PaginaInicial>
+      <PaginaCadastro />
     </>
   );
 }
