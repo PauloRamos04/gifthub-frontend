dcf6ac7e1b25ccf1c52cafd43fca1845f374170a
dryroque <122841769+dryroque@users.noreply.github.com>
2024-05-31 12:08:38 -0300

    Upadate App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import PaginaInicial from './Paginas/PaginaInicial/PaginaInicial';
 import PaginaCadastro from './Paginas/PaginaCadastro/PaginaCadastro';
 import PaginaLogin from './Paginas/PaginaLogin/PaginaLogin';
 import PaginaCarrinho from './Paginas/PaginaCarrinho/PaginaCarrinho';
+import FinalizarCompra from './Paginas/FinalizarCompra/FinalizarCompra';
 import NavBar from './Componentes/Menu/NavBar';
 
 export default function App() {
   return (
     <>
-      <PaginaCarrinho />
+      <FinalizarCompra />
     </>
   );
 }
